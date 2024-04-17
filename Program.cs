b829484149283b7c8b53fb6fe52f1a86bba34d97
adembinaer <50671689+adembinaer@users.noreply.github.com>
2024-04-17 11:39:28 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Udemy_Bootcamp2023_Jannick_Leismann
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
