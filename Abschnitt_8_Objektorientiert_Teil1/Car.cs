e9e4567f9118b436f8880ce29f98f7ac24caf903
adembinaer <50671689+adembinaer@users.noreply.github.com>
2024-05-24 11:46:45 +0200

    Klasse erstellen

---
+++ Abschnitt_8_Objektorientiert_Teil1/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abschnitt_8_Objektorientiert_Teil1
+{
+    internal class Car
+    {
+        string brand;
+        string model;
+        int year;
+    }
+}
---
+++ Abschnitt_8_Objektorientiert_Teil1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abschnitt_8_Objektorientiert_Teil1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
