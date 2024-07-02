e45935c09384dd4ad862ae129106b6c496bc1ec6
adembinaer <50671689+adembinaer@users.noreply.github.com>
2024-07-02 12:40:39 +0200

    Abschnitt_9_ ProjektStart_Bankkonto

---
+++ Abschnitt_9_OOP_Projekt_Bankkonto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abschnitt_9_OOP_Projekt_Bankkonto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
