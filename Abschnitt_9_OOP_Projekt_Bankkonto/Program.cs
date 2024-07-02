98fd4b001d8d9f4a261efa5779b564545d698fb8
adembinaer <50671689+adembinaer@users.noreply.github.com>
2024-07-02 21:18:32 +0200

    Add Menu UI without implemantation

--- Abschnitt_9_OOP_Projekt_Bankkonto/Program.cs
+++ Abschnitt_9_OOP_Projekt_Bankkonto/Program.cs
@@ -6,7 +6,41 @@ namespace Abschnitt_9_OOP_Projekt_Bankkonto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount bankAccount = new BankAccount(
+                "201508",
+                "Ado J",
+                500);
+
+            while (true)
+            {
+                Console.WriteLine("Was möchten Sie tun?");
+                Console.WriteLine("1: Kontostand anzeigen");
+                Console.WriteLine("2: Einzahlung machen");
+                Console.WriteLine("3: Auszahlung machen");
+                Console.WriteLine("4: Beenden");
+                string input = Console.ReadLine();
+
+                if(input == "1")
+                {
+
+                }
+                else if (input == "2")
+                {
+
+                }
+                else if (input == "3")
+                {
+
+                }
+                else if (input == "4")
+                {
+                    break; // Return?
+                }
+                else
+                {
+                    Console.WriteLine("Ungültige Auswahl, bitte versuchen Sie es nochmals");
+                }
+            }
         }
     }
 }
