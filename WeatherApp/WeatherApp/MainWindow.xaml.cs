96d22b0432de269cc3e574aede5a5524dd939815
adembinaer <50671689+adembinaer@users.noreply.github.com>
2024-08-13 13:29:11 +0200

    Add: HttpClient and HttpResponseMessage

--- WeatherApp/WeatherApp/MainWindow.xaml.cs
+++ WeatherApp/WeatherApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,25 @@ namespace WeatherApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly string apiKey = "daa165c2d50865a2078272770f6cadc8";
+
+        private readonly string requestUrl = "http://api.openweathermap.org/geo/1.0/direct"; // Direkt von OpenWeather "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
         public MainWindow()
         {
             InitializeComponent();
+
+            HttpClient httpClient = new HttpClient();
+
+            var city = "Berlin";
+            var finalUri = requestUrl + "?q=" + city + "&appid" + apiKey;
+
+            // Antwort vom Server HttpClient GetAsync() auf Fragestellung von HttpResponseMessage 
+            HttpResponseMessage httpResponse = httpClient.GetAsync(finalUri).Result; //GetAsync = Neue Thread läuft asynchron weiter
+            //Testlauf was erhalten von httpResponse
+            string response = httpResponse.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(response);
+
         }
     }
 }
