febe49968b9c468767b4152e1f1c322207236dbe
adembinaer <50671689+adembinaer@users.noreply.github.com>
2024-08-29 10:56:06 +0200

    Start Lession 12 Dictionary

---
+++ Abschnnitt_12_Dictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Abschnnitt_12_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dictionaries
+
+            // English | Deutsch
+            // Cart | Warenkorb
+            
+
+            // Produkt | Preis
+            // Milch | 1.49f
+
+            // ID | Person (Objekt)
+            // 50 | Objekt maybe Property "Jannick"
+
+
+        }
+    }
+}
