e166f69f1d593657c8666c2bed92abadceba51c8
adembinaer <50671689+adembinaer@users.noreply.github.com>
2024-08-29 12:48:31 +0200

    Lession end part 1

--- Abschnnitt_12_Dictionary/Program.cs
+++ Abschnnitt_12_Dictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Abschnnitt_12_Dictionary
 {
@@ -10,15 +11,29 @@ namespace Abschnnitt_12_Dictionary
 
             // English | Deutsch
             // Cart | Warenkorb
-            
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("Cart", "Warenkorb");
+            dic.Remove("Cart");
 
             // Produkt | Preis
             // Milch | 1.49f
+            Dictionary<string, float> products = new Dictionary<string, float>();
+            products.Add("Milch", 1.49f);
+            products.Remove("Milch");
 
             // ID | Person (Objekt)
             // 50 | Objekt maybe Property "Jannick"
+            Dictionary<int, Person> people = new Dictionary<int, Person>();
+            people.Add(50, new Person());
+            people.Remove(50);
+
+            people.Clear();
 
 
+        }
+        public class Person
+        {
+            // Properties ID und Name ??? 
         }
     }
 }
