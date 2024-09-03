18e6870fb3b1c2a9ff2975afa9cb23ce4712f5e4
adembinaer <50671689+adembinaer@users.noreply.github.com>
2024-09-03 19:59:39 +0200

    Finish Lession Switch/Enum

---
+++ Abschnitt_13_Switch_Enum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Abschnitt_13_Switch_Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Enums Übung
+
+            //Resolutions myResolution = Resolutions.UHD;
+
+            //myResolution = (Resolutions)720;
+
+            //Console.WriteLine(myResolution);
+            ////Übung fertig
+
+            Console.WriteLine("Wählen Sie eine Auflösung (720, 1920, 3840)");
+
+            int resolution = int.Parse(Console.ReadLine());
+
+            Resolutions myResolution = (Resolutions)resolution;
+
+            switch (myResolution)
+            {
+                case Resolutions.SD:
+                    Console.WriteLine("Standard");
+                    break;
+                case Resolutions.HD:
+                    Console.WriteLine("High Definition");
+                    break;
+                case Resolutions.UHD:
+                    Console.WriteLine("Ultra High Definition");
+                    break;
+                default:
+                    Console.WriteLine("Ungültige Eingabe");
+                    break;
+            }
+        }
+    }
+}
---
+++ Abschnitt_13_Switch_Enum/Resolutions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abschnitt_13_Switch_Enum
+{
+    internal enum Resolutions
+    {
+        SD = 720,
+        HD = 1920,
+        UHD = 3840
+    }
+}
