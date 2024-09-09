0574313160efd98bcfb16c4d7a37f019ea02c0ad
adembinaer <50671689+adembinaer@users.noreply.github.com>
2024-09-09 21:15:21 +0200

    Add: create files and read/write, copy files, delete,

---
+++ Abschnitt_15_IO_Files_Path/Directory_Lession.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Abschnitt_15_IO_Files_Path
+{
+    public class Directory_Lession
+    {
+        string path = "files/";
+        string fileName = "text.txt";
+
+        public void DirectoryPath()
+        {
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine("Ordner existiert ");
+            }
+            else
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            var filesInDir = Directory.GetFiles(path);
+            
+        
+        }
+    }
+}
---
+++ Abschnitt_15_IO_Files_Path/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO; // Input/Output
+
+namespace Abschnitt_15_IO_Files_Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string path = "test.txt";
+
+            //// Create File
+            //File.Create(path);
+
+            // Read File
+            //string content = File.ReadAllText(path);
+            //Console.WriteLine(content);
+
+            // Copy File
+
+            //if (File.Exists(path))
+            //{
+            //    string destpath = "testCopy.txt";
+            //    File.Copy(path, destpath);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Can not find file");
+
+            //    FileStream fs = File.Create(path);
+            //    fs.Close();
+
+            //    Console.WriteLine("Created file");
+            //    File.WriteAllText(path, "Hoi zämme"); //File-Inhalt ändern   
+            //}
+
+            // Append -> vorhandenen Text anfügen
+            //if (File.Exists(path))
+            //{
+            //    File.AppendAllText(path, "DUUUUU NO NO NO");
+            //}
+
+            // Delete
+            //if (File.Exists(path))
+            //{
+            //    Console.WriteLine("File deleted");
+            //    File.Delete(path);
+            //}
+
+
+            // Übung Ordner class Directory
+            Directory_Lession diractoryMethod = new Directory_Lession();
+            Console.WriteLine(diractoryMethod.DirectoryPath());
+        }
+    }
+}
+
